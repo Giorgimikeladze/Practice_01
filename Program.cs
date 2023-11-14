3a60d881fd84e9b19b3718f37a61ba029ca63dd9
mikel <mikel@DESKTOP-96M7UL8>
2023-11-15 00:01:05 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+int input;
+Console.WriteLine("Enter Integer number: ");
+string temp=Console.ReadLine();
+
+bool IsParsed=Int32.TryParse(temp, out input);
+
+if (IsParsed)
+{
+    bool IsOdd = input % 2 == 1;
+    if (IsOdd)
+    {
+        Console.WriteLine($"Entered number {input} Is Odd");
+    }
+    else {
+        Console.WriteLine($"Entered number {input} Is not Odd, it's even");
+    }
+}
+else {
+    Console.WriteLine("Invalid Input");
+    }
